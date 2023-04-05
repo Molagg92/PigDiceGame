ac6d07a73b6e7556e12d523f36488ae9b03819ba
Erik Z <molagg92@gmail.com>
2023-04-05 14:09:53 -0700

    add more JS to ur Buisness logic side, we created a fucntion to add all the numbers in our array togetehr, giving us our Total Score of player 1.
    Co-authored by : Kymani Stephens <kymanist00@gmail.com>

--- script.js
+++ script.js
@@ -2,25 +2,45 @@
 
 // Game functions
 
+// Initialize Global Variables
+let player1Score = 0
+let player2Score = 0
 let scoreArray = []
-function playRound() {
-    const num = generateRoll()
-    scoreArray.push(num)
-    console.log(scoreArray) 
-}
-
-
-
 
 // Function generates random number between 1 - 6 returns value
 function generateRoll() {
-let num = Math.floor(Math.random() * 6) + 1   
-// return Math.round(num)
-return num
+    let num = Math.floor(Math.random() * 6) + 1   
+    // return Math.round(num)
+    return num
 }
 
+// Pushes dice roll into round array
+function playRound() {
+    scoreArray.push(generateRoll())
+    console.log(scoreArray)
+    console.log(checkLastRoll())
+}
 
+// Adds zero to round array when you click hold
+function holdRound() {
+    scoreArray.push(0)
+    console.log((scoreArray)) 
+    console.log(checkLastRoll()) 
+}
 
+// Function to check last item of score Array
+function checkLastRoll() {
+    const lastNum = scoreArray.slice(-1)[0]
+    return lastNum
+}
+
+function sumArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]; 
+    } 
+    return sum
+}
 
 
 
@@ -43,3 +63,7 @@ class Player {
 // roll btn player1
 const rollBtn = document.getElementById("roll-btn")
 rollBtn.addEventListener("click", playRound)
+
+// hold btn player1
+const holdBtn = document.getElementById("hold-btn")
+holdBtn.addEventListener("click", holdRound)
